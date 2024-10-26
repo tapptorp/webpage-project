7277f6aaa620a5e8cebddf618e684671bad88e78
Ruben Täpptorp <ruben.tapptorp@gmail.com>
2024-10-26 19:43:06 +0200

    commit vid 38 min, fixar hamburgermenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import { brainwave } from "../assets";
+import { useLocation } from "react-router-dom";
 import { mageseekergaren } from "../assets";
 import { navigation } from "../constants";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(true);
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
+      {" "}
+      {/*w-full gör nav utdraget över hela */}
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img
@@ -14,7 +27,11 @@ const Header = () => {
             alt="MageseekerGaren"
           />
         </a>
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -22,13 +39,20 @@ const Header = () => {
                 href={item.url}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
             ))}
+
+            <HamburgerMenu />
           </div>
-        </nav>
+        </nav>{" "}
+        {/* Här la han till Sign in button vid 32 min men jag sket i det, tog bara en min*/}
       </div>
     </div>
   );
